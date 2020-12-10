c5fb0c0eba8e2973f8f4e32d164e29e940930f7c
Dianne <dianne.diazm@gmail.com>
2020-12-10 15:47:04 -0300

    cards

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,10 +1,97 @@
 <template>
-  <div>
-   
+<div>
+  <b-navbar toggleable type="dark" variant="dark">
+    <b-navbar-brand href="#">MI APP</b-navbar-brand>
+        <b-navbar-toggle target="navbar-toggle-collapse">
+      <b-navbar-nav>
+        <b-nav-item href="#">Link</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+      <template #default="{ expanded }">
+        <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+        <b-icon v-else icon="chevron-bar-down"></b-icon>
+      </template>
+    </b-navbar-toggle>
+
+    <b-collapse id="navbar-toggle-collapse" is-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item href="#">Link 1</b-nav-item>
+        <b-nav-item href="#">Link 2</b-nav-item>
+        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+
+  <div class="container d-flex my-5">
+    <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2 mr-2"
+  >
+  </b-card>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2 mr-2"
+  >
+  </b-card>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2 mr-2"
+  >
+  </b-card>
   </div>
+
+  <div class="container d-flex my-5">
+    <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2 mr-2"
+  >
+  </b-card>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2 mr-2"
+  >
+  </b-card>
+  <b-card
+    title="Card Title"
+    img-src="https://picsum.photos/600/300/?image=25"
+    img-alt="Image"
+    img-top
+    tag="article"
+    style="max-width: 20rem;"
+    class="mb-2 mr-2"
+  >
+  </b-card>
+  </div>
+</div>
+  
 </template>
 
-<script>
+<script>  
 export default {
   name: 'Layout',
 }
